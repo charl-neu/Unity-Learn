371cd04b84799c1546a3bff11e0cb635445732be
charl-neu <cshenkir@student.neumont.edu>
2026-01-08 11:36:46 -0700

    Unity Basic World

---
+++ Assets/Tank/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] float spawntimer = 1.0f;
+    [SerializeField] GameObject spawnObject;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.IsPressed())
+        {
+            Vector3 position = transform.position;
+            position.x += Random.Range(-5f, 5f);
+            position.z += Random.Range(-5f, 5f);
+            Instantiate(spawnObject, position, transform.rotation);
+        }
+    }
+}
